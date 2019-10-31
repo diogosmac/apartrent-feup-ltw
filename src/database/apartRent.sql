b31a2a9a6a0feace5c7fc292017f01a2b393a947
PedroBaptista3622 <ze.pedro3622@outlook.pt>
2019-10-31 14:09:46 +0000

    Database initial files

---
+++ src/database/apartRent.sql
@@ -0,0 +1,31 @@
+DROP TABLE IF EXISTS User;
+
+CREATE TABLE User(
+    username VARCHAR PRIMARY KEY,
+    password VARCHAR NOT NULL,
+    name VARCHAR NOT NULL
+);
+
+DROP TABLE IF EXISTS Apartment;
+
+CREATE TABLE Apartment(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    address VARCHAR NOT NULL,
+    postal_code CHAR(8) NOT NULL,
+    daily_price REAL NOT NULL,
+    description TEXT,
+    owner VARCHAR REFERENCES User(username),
+    CHECK(daily_price > 0),
+    CHECK(postal_code LIKE '____-___')
+);
+
+DROP TABLE IF EXISTS Rental;
+
+CREATE TABLE Rental(
+    apartmentID INTEGER REFERENCES Apartment(id),
+    initDate DATE,
+    endDate DATE,
+    userID VARCHAR REFERENCES User(username),
+    PRIMARY KEY(apartmentID, initDate, endDate),
+    CHECK(initDate < endDate)
+);
