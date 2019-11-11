9319552eb9fd89daf210caf1dc450bdef7dec23f
Joao Lirio <up201705254@fe.up.pt>
2019-11-11 22:32:29 +0000

    Login and signup php

---
+++ src/signup.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<html>
+    <head>
+        <title>Sign Up</title>
+        <meta charset="UTF-8">
+    </head>
+
+    <body>
+        <header>Sign Up</header>
+
+        <div class="container">
+            <input type="email" name="email" placeholder="Email" required>
+            <input type="text" name="username" placeholder="Username" required>
+            <input type="password" name="password" placeholder="Password" required>
+            <input type="submit" value="Login">
+        </div>
+
+        <a href="login.php"> Log In </a>
+    </body>
+</html>
