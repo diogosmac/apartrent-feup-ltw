0082189c2aadbd00e070d679e8717707c8995d07
PedroBaptista3622 <ze.pedro3622@outlook.pt>
2019-11-23 23:53:12 +0000

    Signup css done for now too

--- src/signup.php
+++ src/signup.php
@@ -4,16 +4,17 @@
     <head>
         <title>Sign Up</title>
         <meta charset="UTF-8">
+        <link href="css/signup.css" rel="stylesheet">
     </head>
 
     <body>
         <header>Sign Up</header>
 
         <div class="container">
-            <input type="email" name="email" placeholder="Email" required>
-            <input type="text" name="username" placeholder="Username" required>
-            <input type="password" name="password" placeholder="Password" required>
-            <input type="submit" value="Login">
+            <div class="Email"><input type="email" name="email" placeholder="Email" required></div>
+            <div class="Username"><input type="text" name="username" placeholder="Username" required></div>
+            <div class="Password"><input type="password" name="password" placeholder="Password" required></div>
+            <div class="Register"><input type="submit" value="Submit"></div>
         </div>
 
         <a href="login.php"> Log In </a>
