9f94c9409bc442f9a43dfeeee82eace4f23b75e3
PedroBaptista3622 <ze.pedro3622@outlook.pt>
2019-11-24 20:54:48 +0000

    Profile html

--- src/profile.php
+++ src/profile.php
@@ -8,7 +8,19 @@
 </head>
 
 <body>
-    
+    <div class="main-container">
+        <div class="profile_picture"><img src="https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909__340.png"></div>
+        <div class="name"><h1>Name</h1></div>
+        <div class="description"><article>description</article></div>
+    </div>
+
+    <div class="left-menu">
+        <div class="edit_profile"><a href="edit_profile.php">Edit Profile</a></div>
+        <div class="add_listings"><a href="add_listings.php">Add Listings</a></div>
+        <div class="view_listings"><a href="view_listings.php">My Listings</a></div>
+        <div class="view_rentals"><a href="view_rentals.php">My Rentals</a></div>
+    </div>
+
 </body>
 
 </html>
