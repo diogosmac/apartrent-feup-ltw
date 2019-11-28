15b425c22bb29ca8da1ec83a8a32d8fe329c314d
PedroBaptista3622 <ze.pedro3622@outlook.pt>
2019-11-28 09:38:27 +0000

    Something

--- src/search.php
+++ src/search.php
@@ -15,7 +15,7 @@
     <section id='search_results'>
         <div id='search_parameters'>
             Search results for:
-            <span id='search_place'> Porto </span>
+            <span id='search_place'>Porto</span>
             <span id='search_begin_date'>10/10/2019</span>
             <span id='search_end_date'>12/10/2019</span>
         </div>
@@ -25,7 +25,7 @@
             <img src="https://images.unsplash.com/photo-1512917774080-9991f1c4c750?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80" alt="">
             <section class='info'>
                 <span id='nome'> Maison de CAL </span>
-                <span id='preco'> 40 </span>
+                <span id='preco'> 40</span>
                 <span id='max_pessoas'> 4 </span>
                 <span id='localidade'> Arcos de Valdevez </span>
                 <span id='rating'> 4.9 </span>
@@ -36,7 +36,7 @@
             <img src="https://images.unsplash.com/photo-1527030280862-64139fba04ca?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2106&q=80" alt="">
             <section class='info'>
                 <span id='nome'> Maison de CAL </span>
-                <span id='preco'> 40 </span>
+                <span id='preco'> 40</span>
                 <span id='max_pessoas'> 4 </span>
                 <span id='localidade'> Arcos de Valdevez</span>
                 <span id='rating'> 5.1 </span>
