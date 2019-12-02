c980f6670b970778d873d359ede5f057ca1a3702
Joao Lirio <up201705254@fe.up.pt>
2019-12-02 23:14:19 +0000

    Edit profile done for now

--- src/edit_profile.php
+++ src/edit_profile.php
@@ -12,11 +12,17 @@
     
         <div class="main-container">
             <div class="profile_picture">
-                <img src="https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909__340.png">
-                <button id="profile-pic-upload" type="button">Change photo</button>
+                <div class="image"> <img src="https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909__340.png"> </div>
+                <div class="button"> <button id="profile-pic-upload" type="button">Change photo</button> </div>
             </div>
 
-            <div class="description"> <textarea id="about" name="description" rows="5"></textarea> </div>
+            <div class="description">
+                <div><label for="description">Description</label></div> 
+                <div><textarea id="about" name="description" rows="5"></textarea></div> 
+            </div>
         </div>
+
+        <?php include('templates/common/footer.php') ?>
+
     </body>
 </html>
