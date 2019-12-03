43d1cfe9ba2115c79d3f6e42c40aa9c831fec2b2
diogo <diogo.smac@gmail.com>
2019-12-03 11:31:58 +0000

    Profile editing page tweaks

--- src/editProfile.php
+++ src/editProfile.php
@@ -1,3 +1,6 @@
 <?php
-
+    $title = "Edit profile";
+    include_once('templates/common/header.php');
+    include_once('templates/user/editProfile.php');
+    include_once('templates/common/footer.php');
 ?>
---
+++ src/templates/user/editProfile.php
@@ -0,0 +1,18 @@
+<div class="edit-profile-container">
+    <div class="edit-personals">
+        <div class="profile-picture">
+            <div class="image"><img src="https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909__340.png"> </div>
+            <div class="button"><button id="profile-pic-upload" type="button">Change photo</button> </div>
+        </div>
+        <div class="account-details">
+            <div class="username"><input type="text" name="username" placeholder="New Username"></div>
+            <div class="password"><input type="password" name="password" placeholder="New Password"></div>
+            <div class="confirm-password"><input type="password" name="confirm_password" placeholder="Confirm Password"></div>
+            <div class="button"><button id="update-profile" type="button">Confirm</button></div>
+        </div>
+    </div>
+    <div class="description">
+        <div><label for="description">Description</label></div> 
+        <div><textarea id="about" name="description" rows="5"></textarea></div> 
+    </div>
+</div>
