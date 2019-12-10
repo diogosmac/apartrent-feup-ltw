38e364649756e4fa3d13c25ec35d84dc6003a707
PedroBaptista3622 <ze.pedro3622@outlook.pt>
2019-12-10 19:20:18 +0000

    Quick bug fix and refactor

--- src/actions/editProfileAction.php
+++ src/actions/editProfileAction.php
@@ -5,35 +5,8 @@
   include_once('../actions/validSession.php');
   include_once ('../database/photo.php');
 
-  if($_FILES["profile_pic"]["size"] == 0 && $_FILES["profile_pic"]["tmp_name"] == NULL) //No file specified
-  {
-    print("File not specified");
-  }
-  else
-  {
-    $oldPhotoID = getPhotoID($userID);
-
-    addNewPhoto();
-
-    global $db;
-    
-    // Get image ID
-    $id = $db->lastInsertId();
-
-    // Generate filenames
-    $originalFileName = "../uploadedImages/$id.jpg";
-
-    // Move the uploaded file to its final destination
-    move_uploaded_file($_FILES["profile_pic"]["tmp_name"], $originalFileName);
-
-    updateProfilePhoto($userID, $id);
-    $_SESSION['profile_picture'] = getPhotoPath($id);
-
-    //Deletes old profile photo, to save some space
-    $oldPhotoPath = getPhotoPath($oldPhotoID['idPhoto']);
-    unlink($oldPhotoPath);
-  }
-
+  if(!($_FILES["profile_pic"]["size"] == 0 && $_FILES["profile_pic"]["tmp_name"] == NULL)) //No file specified
+    include_once('../actions/upload.php');
 
   header("Location: ../pages/profile.php");
 ?>
