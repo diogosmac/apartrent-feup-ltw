ef8405a616482ca3d3a7ef7c475d1d3622aacd47
PedroBaptista3622 <ze.pedro3622@outlook.pt>
2019-12-14 16:02:58 +0000

    Add listing option added, new window, and it closes too!

--- src/js/listing.js
+++ src/js/listing.js
@@ -1,9 +1,52 @@
 var editListing = function(){
     var page = document.getElementById("profile-container");
 
-    page.innerHTML += '<div class="ModalBox" id="modalBox"><div class="ModalContent"><div class="ModalCloseBar"><i class="fas fa-times" onclick=closeEdit()></i></div><p>Please enter the fields you want to edit </p><form id="apartmentInfo"><input type="text" placeholder="New Listing Name"><input type="number" step="1" min="1" max="20" id="newNumberGuests" placeholder="New number guests"><input type="number" step="0.1" min="1" max="100000" id="newPricePerDay" placeholder="New price per day"><textarea id="newDescription" rows="3" placeholder="New Description"></textarea><button id="submit">Submit</button></form></div></div>';
+    page.innerHTML += `
+            <div class="ModalBox" id="modalBoxEdit">
+            <div class="ModalContent">
+                <div class="ModalCloseBar"><i class="fas fa-times" onclick=closeEdit()></i></div>
+                <p>Please enter the fields you want to edit </p>
+                <form id="apartmentInfo">
+                    <input type="text" placeholder="New Listing Name">
+                    <input type="number" step="1" min="1" max="20" id="newNumberGuests" placeholder="New number guests">
+                    <input type="number" step="0.1" min="1" max="100000" id="newPricePerDay" placeholder="New price per day">
+                    <textarea id="newDescription" rows="3" placeholder="New Description"></textarea>
+                    <button id="submit">Submit</button>
+                </form>
+            </div>
+        </div>
+    `;
 };
 
 var closeEdit = function(){
-    document.getElementById("modalBox").remove();
+    document.getElementById("modalBoxEdit").remove();
+};
+
+var addListing = function()
+{
+    var page = document.getElementById("profile-container");
+
+    page.innerHTML += `
+            <div class="ModalBox" id="modalBoxAdd">
+            <div class="ModalContent">
+                <div class="ModalCloseBar"><i class="fas fa-times" onclick=closeAdd()></i></div>
+                <p>Please enter the fields you want to edit </p>
+                <form id="apartmentInfo">
+                    <input type="text" placeholder="Listing Name">
+                    <input type="text" placeholder="Locale">
+                    <input type="text" placeholder="Address">
+                    <input type="text" placeholder="Postal Code" pattern="[0-9][0-9][0-9][0-9]-[0-9][0-9][0-9]" title="NNNN-NNN, N = number">
+                    <input type="number" step="1" min="1" max="20" id="newNumberGuests" placeholder="Number guests">
+                    <input type="number" step="0.1" min="1" max="100000" id="newPricePerDay" placeholder="Price per day">
+                    <textarea id="newDescription" rows="3" placeholder="New Description"></textarea>
+                    <button id="submit">Submit</button>
+                </form>
+            </div>
+        </div>
+    `;
+};
+
+var closeAdd = function closeAdd()
+{
+    document.getElementById("modalBoxAdd").remove();
 };
--- src/templates/listings.php
+++ src/templates/listings.php
@@ -23,20 +23,55 @@ include_once('../database/photo.php');
         $query_results = getUserListings($userID);
         if (count($query_results) == 0) {
             echo '<p> No results found. </p>';
-        } else {
-            foreach ($query_results as $apartmentID) {
+        } 
+        else 
+        {
+            foreach ($query_results as $apartmentID) 
+            {
                 showListing($apartmentID);
             }
         }
-        ?>
-    </section>
 
-    <!-- <div class="ModalBox">
-        <div class="ModalContent">
-            <div class="ModalCloseBar"><i class="fas fa-times" onclick=closeEdit()></i></div>
-            <p>Please enter the fields you want to edit </p>
-            <form id="apartmentInfo"><input type="text" placeholder="New Listing Name"><input type="number" step="1" min="1" max="20" id="newNumberGuests" placeholder="New number guests"><input type="number" step="0.1" min="1" max="100000" id="newPricePerDay" placeholder="New price per day"><textarea id="newDescription" rows="3" placeholder="New Description"></textarea><button id="submit">Submit</button></form>
+        if($_GET['manage'] == 1)
+            echo '<article id="addNewListing" onclick="addListing();">
+                    <i class="fas fa-plus-circle"></i>
+                    <span>Add new</span>
+                </article>';
+
+        ?>
+<!-- 
+        <div class="ModalBox" id="modalBoxEdit">
+            <div class="ModalContent">
+                <div class="ModalCloseBar"><i class="fas fa-times" onclick=closeEdit()></i></div>
+                <p>Please enter the fields you want to edit </p>
+                <form id="apartmentInfo">
+                    <input type="text" placeholder="Listing Name">
+                    <input type="text" placeholder="Locale">
+                    <input type="text" placeholder="Address">
+                    <input type="text" placeholder="Postal Code" pattern="[0-9][0-9][0-9][0-9]-[0-9][0-9][0-9]" title="NNNN-NNN, N = number">
+                    <input type="number" step="1" min="1" max="20" id="newNumberGuests" placeholder="Number guests">
+                    <input type="number" step="0.1" min="1" max="100000" id="newPricePerDay" placeholder="Price per day">
+                    <textarea id="newDescription" rows="3" placeholder="New Description"></textarea>
+                    <button id="submit">Submit</button>
+                </form>
+            </div>
         </div>
-    </div> -->
+
+
+        <div class="ModalBox" id="modalBoxAdd">
+            <div class="ModalContent">
+                <div class="ModalCloseBar"><i class="fas fa-times" onclick=closeAdd()></i></div>
+                <p>Please enter the fields you want to edit </p>
+                <form id="apartmentInfo">
+                    <input type="text" placeholder="New Listing Name">
+                    <input type="number" step="1" min="1" max="20" id="newNumberGuests" placeholder="New number guests">
+                    <input type="number" step="0.1" min="1" max="100000" id="newPricePerDay" placeholder="New price per day">
+                    <textarea id="newDescription" rows="3" placeholder="New Description"></textarea>
+                    <button id="submit">Submit</button>
+                </form>
+            </div>
+        </div> -->
+
+    </section>
 
 </div>
