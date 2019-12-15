10a999d2a0bd7905c82196b3e3ce80652d0ae72c
PedroBaptista3622 <ze.pedro3622@outlook.pt>
2019-12-15 01:03:11 +0000

    Add form a bit changed, photos are not being stored yet

--- src/templates/listings.php
+++ src/templates/listings.php
@@ -39,39 +39,44 @@ include_once('../database/photo.php');
                 </article>';
 
         ?>
-<!-- 
-        <div class="ModalBox" id="modalBoxEdit">
-            <div class="ModalContent">
-                <div class="ModalCloseBar"><i class="fas fa-times" onclick=closeEdit()></i></div>
-                <p>Please enter the fields you want to edit </p>
-                <form id="apartmentInfo">
-                    <input type="text" placeholder="Listing Name">
-                    <input type="text" placeholder="Locale">
-                    <input type="text" placeholder="Address">
-                    <input type="text" placeholder="Postal Code" pattern="[0-9][0-9][0-9][0-9]-[0-9][0-9][0-9]" title="NNNN-NNN, N = number">
-                    <input type="number" step="1" min="1" max="20" id="newNumberGuests" placeholder="Number guests">
-                    <input type="number" step="0.1" min="1" max="100000" id="newPricePerDay" placeholder="Price per day">
-                    <textarea id="newDescription" rows="3" placeholder="New Description"></textarea>
-                    <button id="submit">Submit</button>
-                </form>
-            </div>
-        </div>
 
+    </section>
 
-        <div class="ModalBox" id="modalBoxAdd">
+        <!-- <div class="ModalBox" id="modalBoxAdd">
             <div class="ModalContent">
                 <div class="ModalCloseBar"><i class="fas fa-times" onclick=closeAdd()></i></div>
-                <p>Please enter the fields you want to edit </p>
-                <form id="apartmentInfo">
-                    <input type="text" placeholder="New Listing Name">
-                    <input type="number" step="1" min="1" max="20" id="newNumberGuests" placeholder="New number guests">
-                    <input type="number" step="0.1" min="1" max="100000" id="newPricePerDay" placeholder="New price per day">
-                    <textarea id="newDescription" rows="3" placeholder="New Description"></textarea>
-                    <button id="submit">Submit</button>
+                <p>Please fill the form about your apartment</p>
+                <form id="apartmentInfo" method="Post" action="../actions/addListingAction.php">
+                <p>Photos (min 1)</p>
+                    <div class="ApartmentPhotos">
+                        <label id="photo1" for="photo1I"><i class="fas fa-plus-circle"></i></label>
+                        <input name="photo1" id="photo1I" style="visibility:hidden;" type="file" required>
+
+                        <label id="photo2" for="photo2I"><i class="fas fa-plus-circle"></i></label>
+                        <input name="photo2" id="photo2I" style="visibility:hidden;" type="file">
+
+                        <label id="photo3" for="photo3I"><i class="fas fa-plus-circle"></i></label>
+                        <input name="photo3" id="photo3I" style="visibility:hidden;" type="file">
+
+                        <label id="photo4" for="photo4I"><i class="fas fa-plus-circle"></i></label>
+                        <input name="photo4" id="photo4I" style="visibility:hidden;" type="file">
+
+                        <label id="photo5" for="photo5I"><i class="fas fa-plus-circle"></i></label>
+                        <input name="photo5" id="photo5I" style="visibility:hidden;" type="file">
+
+                        <label id="photo6" for="photo6I" ><i class="fas fa-plus-circle"></i></label>
+                        <input name="photo6" id="photo6I" style="visibility:hidden;" type="file">
+                    </div>
+                    <input type="text" name="listingName-Add" placeholder="Listing Name" required>
+                    <input type="text" name="locale-Add" placeholder="Locale" required>
+                    <input type="text" name="address-Add" placeholder="Address" required>
+                    <input type="text" name="postalCode-Add" placeholder="Postal Code" pattern="[0-9][0-9][0-9][0-9]-[0-9][0-9][0-9]" title="NNNN-NNN, N = number" required>
+                    <input type="number" name="nGuests-Add" step="1" min="1" max="20" id="newNumberGuests" placeholder="Number guests" required>
+                    <input type="number" name="price-Add" step="1" min="1" max="100000" id="newPricePerDay" placeholder="Price per day" required>
+                    <textarea id="newDescription" name="description-Add" rows="3" placeholder="New Description"></textarea>
+                    <button name="submit-Add">Add!</button>
                 </form>
             </div>
         </div> -->
-
-    </section>
 
 </div>
