31f2040be6c1da4b434077a8d24e7acb589c05cf
PedroBaptista3622 <ze.pedro3622@outlook.pt>
2019-12-16 22:42:31 +0000

    Merge

--- src/actions/showEditListing.php
+++ src/actions/showEditListing.php
@@ -32,7 +32,7 @@ $return = '<div class="ModalContent">
             $return .= '</div>
         <input type="hidden" name="idApartment" value=' . $apartID . '>
         <input type="text" name="listingName-Edit" placeholder="New Listing Name" 
-               pattern="^(?=. {1,40}$)(?!.*[-] {2})[a-zA-Z][a-zA-Z0-9._- ]*[a-zA-Z0-9]$"
+               pattern="[a-zA-Z- ]{1,40}"
                title="The name has to be between 1 and 40 characters long. You can not use special characters other than ., _ and -. You can\'t have more than one of these characters in a row. You also can\'t start or finish the name with a special character">
         <input type="number" name="price-Edit" step="0.1" min="1" max="100000" id="newPricePerDay" placeholder="New price per day">
         <input type="number" name="nGuests-Edit" step="1" min="1" max="20" id="newNumberGuests" placeholder="New number guests">
