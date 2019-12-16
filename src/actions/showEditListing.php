6daefb5c3e0c486c74b50c9b0364df7b467d83df
Diogo Machado <diogo.smac@gmail.com>
2019-12-16 22:41:04 +0000

    Bugfix on edit listing

--- src/actions/showEditListing.php
+++ src/actions/showEditListing.php
@@ -5,6 +5,9 @@ include_once('../database/db_apartRent.php');
 include_once('../database/photo.php');
 
 $apartID = $_GET['apartID'];
+$apartmentPhotos = getApartmentPhotos($apartID);
+$nRemainingSlots = 6 - count($apartmentPhotos);
+$photoNumber = count($apartmentPhotos) + 1;
 
 $return = '<div class="ModalContent">
     <div class="ModalCloseBar"><i class="fas fa-times" onclick=closeEdit(this)></i></div>
@@ -12,21 +15,18 @@ $return = '<div class="ModalContent">
     <form id="apartmentInfo" method="POST" action="../actions/editListingAction.php" enctype="multipart/form-data">
         <div class="ApartmentPhotos">';
 
-            $apartmentPhotos = getApartmentPhotos($apartID);
             foreach ($apartmentPhotos as $apartmentPhoto) {
-            $photoPath = getPhotoPath($apartmentPhoto['idPhoto']);
-            $return .= '<img src="' . $photoPath . '">';
+                $photoPath = getPhotoPath($apartmentPhoto['idPhoto']);
+                $return .= '<img src="' . $photoPath . '">';
             }
 
-            $nRemainingSlots = 6 - count($apartmentPhotos);
-            $photoNumber = count($apartmentPhotos) + 1;
             while ($nRemainingSlots != 0) {
-            $return .= '<label id="photo' . $photoNumber . '" for="photo' . $photoNumber . 'I"><i class="fas fa-plus-circle"></i></label>
-            <input name="photo' . $photoNumber . 'I" id="photo' . $photoNumber . 'I" style="visibility:hidden;" type="file">
-            <input type="hidden" name="photoNumber[]" value="' . $photoNumber . '">';
+                $return .= '<label id="photo' . $photoNumber . '" for="photo' . $photoNumber . 'I"><i class="fas fa-plus-circle"></i></label>
+                <input name="photo' . $photoNumber . 'I" id="photo' . $photoNumber . 'I" style="visibility:hidden;" type="file">
+                <input type="hidden" name="photoNumber[]" value="' . $photoNumber . '">';
 
-            $photoNumber++;
-            $nRemainingSlots--;
+                $photoNumber++;
+                $nRemainingSlots--;
             }
 
             $return .= '</div>
