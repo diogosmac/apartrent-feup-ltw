475ea2137c42ca5bee91df0d17cce0f4c0bd9ec7
Diogo Machado <diogo.smac@gmail.com>
2019-12-16 23:55:54 +0000

    Database changes - Ep. 1

--- src/database/populate.sql
+++ src/database/populate.sql
@@ -3,7 +3,7 @@ PRAGMA foreign_keys = ON;
 INSERT INTO
     User (
         username,
-        password,
+        PASSWORD,
         name,
         email,
         description,
@@ -22,7 +22,7 @@ VALUES
 INSERT INTO
     User (
         username,
-        password,
+        PASSWORD,
         name,
         email,
         description,
@@ -31,17 +31,17 @@ INSERT INTO
 VALUES
     (
         'admin',
-        'admin',
-        'NotAdmin',
+        'Admin123',
+        'Definitely Not Admin',
         'admin@admin.com',
-        'Admin acc',
+        'A ginger bread man lives in a ginger bread house. Is the house made of flesh, or is he made of house? He screams, for he does not know.',
         2
     );
 
 INSERT INTO
     User (
         username,
-        password,
+        PASSWORD,
         name,
         email,
         description,
@@ -55,6 +55,44 @@ VALUES
         'up201705255@fe.up.pt',
         NULL,
         3
+    );
+
+INSERT INTO
+    User (
+        username,
+        PASSWORD,
+        name,
+        email,
+        description,
+        idUser
+    )
+VALUES
+    (
+        'Teste',
+        'Teste123',
+        'teste',
+        'teste2@test.pt',
+        NULL,
+        4
+    );
+
+INSERT INTO
+    User (
+        username,
+        PASSWORD,
+        name,
+        email,
+        description,
+        idUser
+    )
+VALUES
+    (
+        'smacc',
+        'DocenteDeCal0',
+        'Diogo Machoado',
+        'address@domain.cal',
+        NULL,
+        5
     );
 
 INSERT INTO
@@ -116,58 +154,268 @@ VALUES
     );
 
 INSERT INTO
-    Rental (
-        apartmentID,
-        initDate,
-        endDate,
-        idUser,
-        rate
-    )
-VALUES
-    (
-        2,
-        '26-11-2019',
-        '28-11-2019',
-        2,
-        NULL
-    );
-
-INSERT INTO
-    Photo (
-        idPhoto
-    )
-VALUES
-    (0);
-
-INSERT INTO
-    [User-Photo] (
-        idUser,
-        idPhoto
-    )
-VALUES
-    (
-        1,
-        0
-    );
-
-INSERT INTO
-    [User-Photo] (
-        idUser,
-        idPhoto
-    )
-VALUES
-    (
-        2,
-        0
-    );
-
-INSERT INTO
-    [User-Photo] (
-        idUser,
-        idPhoto
+    Apartment (
+        id,
+        address,
+        postal_code,
+        daily_price,
+        description,
+        owner,
+        locale,
+        listing_name,
+        n_guests,
+        n_ratings,
+        average_rating
     )
 VALUES
     (
         3,
-        0
+        'Rua Dr. Roberto Frias',
+        '4000-123',
+        30.0,
+        'Soothing from the outside, terrifying from the inside',
+        2,
+        'Porto',
+        'Casa Teste',
+        20,
+        0,
+        0.0
     );
+
+INSERT INTO
+    Rental (apartmentID, initDate, endDate, idUser, Rating)
+VALUES
+    (2, '2019-11-26', '2019-11-28', 2, NULL);
+
+INSERT INTO
+    Rental (apartmentID, initDate, endDate, idUser, Rating)
+VALUES
+    (1, '2019-12-04', '2019-12-06', 2, NULL);
+
+INSERT INTO
+    Rental (apartmentID, initDate, endDate, idUser, Rating)
+VALUES
+    (1, '2019-11-05', '2019-11-07', 2, NULL);
+
+INSERT INTO
+    Rental (apartmentID, initDate, endDate, idUser, Rating)
+VALUES
+    (2, '2019-12-21', '2019-12-22', 2, NULL);
+
+INSERT INTO
+    Rental (apartmentID, initDate, endDate, idUser, Rating)
+VALUES
+    (1, '2019-12-27', '2019-12-30', 2, NULL);
+
+INSERT INTO
+    Rental (apartmentID, initDate, endDate, idUser, Rating)
+VALUES
+    (3, '2019-12-04', '2019-12-06', 2, NULL);
+
+INSERT INTO
+    Rental (apartmentID, initDate, endDate, idUser, Rating)
+VALUES
+    (2, '2019-12-04', '2019-12-06', 2, NULL);
+
+INSERT INTO
+    Photo (idPhoto)
+VALUES
+    (0);
+
+INSERT INTO
+    Photo (idPhoto)
+VALUES
+    (1);
+
+INSERT INTO
+    Photo (idPhoto)
+VALUES
+    (2);
+
+INSERT INTO
+    Photo (idPhoto)
+VALUES
+    (12);
+
+INSERT INTO
+    Photo (idPhoto)
+VALUES
+    (13);
+
+INSERT INTO
+    Photo (idPhoto)
+VALUES
+    (14);
+
+INSERT INTO
+    Photo (idPhoto)
+VALUES
+    (15);
+
+INSERT INTO
+    Photo (idPhoto)
+VALUES
+    (16);
+
+INSERT INTO
+    Photo (idPhoto)
+VALUES
+    (17);
+
+INSERT INTO
+    Photo (idPhoto)
+VALUES
+    (18);
+
+INSERT INTO
+    Comments (apartmentID, numComment, idUser, date_time, text)
+VALUES
+    (
+        1,
+        1,
+        2,
+        '2019-12-12 17:30:00',
+        'Mas que casa mais heterossexual, um petisco, de facto!'
+    );
+
+INSERT INTO
+    Comments (apartmentID, numComment, idUser, date_time, text)
+VALUES
+    (
+        1,
+        2,
+        2,
+        '2019-12-12 17:30:10',
+        'Mas que belo, bem belo mesmo!!'
+    );
+
+INSERT INTO
+    Comments (apartmentID, numComment, idUser, date_time, text)
+VALUES
+    (
+        2,
+        1,
+        2,
+        '2019-12-12 17:30:44',
+        'Bel�ssimo! E ali�s, muito heterossexual!'
+    );
+
+INSERT INTO
+    Comments (apartmentID, numComment, idUser, date_time, text)
+VALUES
+    (
+        2,
+        2,
+        2,
+        '2019-12-12 17:47:11',
+        'Testando o overflow com um berro. Aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaah.'
+    );
+
+INSERT INTO
+    Comments (apartmentID, numComment, idUser, date_time, text)
+VALUES
+    (
+        1,
+        3,
+        1,
+        '2019-12-12 17:54:14',
+        'BEM ALIMENTAAAAAAAAAAAAAAAAAAAAAAAAAAAAADO'
+    );
+
+INSERT INTO
+    Comments (apartmentID, numComment, idUser, date_time, text)
+VALUES
+    (
+        2,
+        3,
+        1,
+        '2019-12-12 17:55:25',
+        'Testando o overflow com um berro. Aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaah.'
+    );
+
+INSERT INTO
+    Comments (apartmentID, numComment, idUser, date_time, text)
+VALUES
+    (
+        1,
+        4,
+        2,
+        '2019-12-12 20:00:54',
+        'AI CHASSUUUS QUE ELE AT� ARRANJA OS BUGS E TUDO MAIS, MAS QUE ADMIN ESTE ADMIN'
+    );
+
+INSERT INTO
+    Comments (apartmentID, numComment, idUser, date_time, text)
+VALUES
+    (
+        3,
+        1,
+        2,
+        '2019-12-16 17:38:51',
+        'Mas que casa de teste, esta casa de teste!'
+    );
+
+INSERT INTO
+    Comments (apartmentID, numComment, idUser, date_time, text)
+VALUES
+    (3, 2, 2, '2019-12-16 17:38:59', 'Beleza!!');
+
+INSERT INTO
+    "Apartment-Photo" (idApartment, idPhoto)
+VALUES
+    (1, 1);
+
+INSERT INTO
+    "Apartment-Photo" (idApartment, idPhoto)
+VALUES
+    (1, 2);
+
+INSERT INTO
+    "Apartment-Photo" (idApartment, idPhoto)
+VALUES
+    (2, 15);
+
+INSERT INTO
+    "Apartment-Photo" (idApartment, idPhoto)
+VALUES
+    (2, 14);
+
+INSERT INTO
+    "Apartment-Photo" (idApartment, idPhoto)
+VALUES
+    (3, 16);
+
+INSERT INTO
+    "Apartment-Photo" (idApartment, idPhoto)
+VALUES
+    (3, 17);
+
+INSERT INTO
+    "Apartment-Photo" (idApartment, idPhoto)
+VALUES
+    (3, 18);
+
+INSERT INTO
+    "User-Photo" (idUser, idPhoto)
+VALUES
+    (1, 0);
+
+INSERT INTO
+    "User-Photo" (idUser, idPhoto)
+VALUES
+    (2, 13);
+
+INSERT INTO
+    "User-Photo" (idUser, idPhoto)
+VALUES
+    (3, 0);
+
+INSERT INTO
+    "User-Photo" (idUser, idPhoto)
+VALUES
+    (4, 0);
+
+INSERT INTO
+    "User-Photo" (idUser, idPhoto)
+VALUES
+    (5, 12);
--- src/database/triggers.sql
+++ src/database/triggers.sql
@@ -1,10 +1,13 @@
 -- Trigger: PhotoUpdated
 DROP TRIGGER IF EXISTS PhotoUpdated;
 CREATE TRIGGER PhotoUpdated
-         AFTER UPDATE OF idPhoto
-            ON [User-Photo]
-BEGIN
-    DELETE FROM Photo
-          WHERE Photo.idPhoto > 0 AND 
-                Old.idPhoto = Photo.idPhoto;
+AFTER
+UPDATE
+      OF idPhoto ON "User-Photo" BEGIN
+DELETE FROM
+      Photo
+WHERE
+      Photo.idPhoto > 0
+      AND Old.idPhoto = Photo.idPhoto;
+
 END;
